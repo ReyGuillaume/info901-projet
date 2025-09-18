bd706d3cb14ec21a7eb808cd2abb8cc8d733d850
WhiLe4ent <achille.gravouil@gmail.com>
2025-09-18 10:58:05 +0200

    WIP

--- Com.py
+++ Com.py
@@ -3,6 +3,7 @@ from Message import Message
 
 from pyeventbus3.pyeventbus3 import *
 from Message import Message, MessageTo
+from threading import Event
 
 class Com():
     NB_PROCESS = 0
@@ -40,9 +41,36 @@ class Com():
         PyBus.Instance().post(msg)
         pass
 
-    def sendToSync(self, message, dest):
 
-        pass
+
+    def sendToSync(self, message, dest):
+        # créer un objet Event pour attendre la réponse
+        ack_event = Event()
+        response_container = {}
+
+        PyBus.Instance().register(handle_ack, handle_ack)
+
+        # envoyer le message
+        msg = MessageTo(self.getMyId(), dest, message)
+        print(f"{self.getMyId()} sendToSync P{dest}: {message}")
+        PyBus.Instance().post(msg)
+
+        # attendre la réponse
+        ack_event.wait(timeout=5)
+
+        return response_container.get('msg', None)
+
+
+    # handler pour réception de l'ack
+    @subscribe(threadMode=Mode.PARALLEL, onEvent=MessageTo)
+    def handle_ack_sync(msg):
+        if msg.getReceiver() == self.getMyId() and msg.getSender() == dest:
+            # stocker la réponse
+            response_container['msg'] = msg
+            # débloquer le wait
+            ack_event.set()
+            # se désabonner après réception
+            PyBus.Instance().unregister(handle_ack)
 
     def recvFromSync(self, source):
         pass
